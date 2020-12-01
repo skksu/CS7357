cb836633e4a9a521e1ef6bdeda11916222efd99d
kevinsuo <suokunstar@gmail.com>
2020-12-01 16:57:05 -0500

    .

--- linear-regression/lr.py
+++ linear-regression/lr.py
@@ -1,38 +1,65 @@
-# Read Auto MPG data
-import pandas as pd
-import numpy as np
-
-data = pd.read_csv('auto-mpg.data.csv')
-
-# make two variables - X and y
-y = data.iloc[:, 0]  # the first columne is for class label
-X = data.drop('mpg', axis=1)
-X = X.drop('carname', axis=1)
-n, p = X.shape # number of samples and features
-X = X.iloc[:, 2] # use only the second feature
-X = pd.DataFrame(np.c_[np.ones(n), X])
-
-# plot the data
+from collections import OrderedDict
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
 import matplotlib.pyplot as plt
-plt.plot(X.iloc[:, 1], y, 'o')
-plt.xlabel("displacement")
-plt.ylabel("MPG")
+import pandas as pd
+
+examDict={
+    'Study time':[0.50,0.75,1.00,1.25,1.50,1.75,1.75,2.00,2.25,
+            2.50,2.75,3.00,3.25,3.50,4.00,4.25,4.50,4.75,5.00,5.50],
+    'Score':    [10,  22,  13,  43,  20,  22,  33,  50,  62,  
+              48,  55,  75,  62,  73,  81,  76,  64,  82,  90,  93]
+}
+examOrderDict=OrderedDict(examDict)
+exam=pd.DataFrame(examOrderDict)
+
+#Export labels and features from DataFrame
+exam_X = exam['Study time']
+exam_Y = exam['Score']
+
+#Draw a scatter chart, judge whether it is suitable for the linear regression model by drawing, and comment out after the judgment
+plt.scatter(exam_X, exam_Y, color = 'green')
+
+#plt.ylabel('Scores')
+#plt.xlabel('Times(h)')
+#plt.title('Exam Data')
+#plt.show()
+
+#The ratio is 7:3
+X_train, X_test, Y_train, Y_test = train_test_split(exam_X,exam_Y, train_size = 0.7)
+
+#Import linear regression model
+#sklearn requires the input features to be a two-dimensional array type.
+#Data set has only 1 feature, you need to use array.reshape(-1, 1) to change the shape of the array
+
+#Change the shape of the array
+X_train = X_train.values.reshape(-1, 1)
+X_test = X_test.values.reshape(-1, 1)
+
+#Create a model
+model = LinearRegression()
+#Train the model
+model.fit(X_train, Y_train)
+
+a = model.intercept_
+b = model.coef_
+a = float(a)
+b = float(b)
+print('The linear regression equation of the model is y = {} + {} * x'.format(a, b))
+
+accuracy = model.score(X_test, Y_test)
+print(accuracy)
+
+#Draw a scatter chart
+plt.scatter(exam_X, exam_Y, color = 'green', label = 'train data')
+#Set X, Y axis label and title
+plt.ylabel('Scores')
+plt.xlabel('Times(h)')
+
+#Draw the best fit curve
+Y_train_pred = model.predict(X_train)
+plt.plot(X_train, Y_train_pred, color = 'black', label = 'best line')
+
+#Output
+plt.legend(loc = 2)
 plt.show()
-
-
-def SolverLinearRegression(X, y):
-    return np.dot(np.dot(np.linalg.inv(np.dot(X.transpose(), X)), X.transpose()), y)
-
-def abline(slope, intercept):
-    axes = plt.gca()
-    x_vals = np.array(axes.get_xlim())
-    y_vals = intercept + slope * x_vals
-    plt.plot(x_vals, y_vals, '--')
-
-b = SolverLinearRegression(X, y)
-print(b)
-
-plt.plot(X.iloc[:, 1], y, 'o')
-abline(b[1], b[0])
-plt.show()
-
--- linear-regression/lr0.py
+++ linear-regression/lr0.py
@@ -1,7 +1,8 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-#loadDataSet()函数打开一个用tab键分隔的文本文件，这里默认文件每行的最后一个值是目标值。
+#The loadDataSet() function opens a text file separated by the tab key, 
+#where the last value of each line of the default file is the target value.
 def loadDataSet(fileName):
     numFeat = len(open(fileName).readline().split('\t')) - 1
     dataMat = []; labelMat = []
@@ -15,9 +16,9 @@ def loadDataSet(fileName):
         labelMat.append(float(curLine[-1]))
     return dataMat,labelMat
 
-#standRegres()函数用于计算最佳拟合直线。
-#该函数首先读入x和y并将它们保存到矩阵中，然后计算xTx，然后判断它的行列式是否为0。
-#如果行列式为0，那么计算逆矩阵的时候将出现错误。
+#The standRegres() function is used to calculate the best fit straight line.
+#This function first reads x and y and saves them in the matrix, then calculates xTx, and then judges whether its determinant is 0.
+#If the determinant is 0, an error will occur when calculating the inverse matrix.
 def standRegres(xArr,yArr):
     xMat = np.mat(xArr); yMat = np.mat(yArr).T
     xTx = xMat.T*xMat
